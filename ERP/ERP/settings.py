8a232a68ed0c8852522e4f660cef5b3149e62ead
Atul12396 <atul66017@gmail.com>
2025-04-19 12:51:38 +0530

    changes in the settings

--- ERP/ERP/settings.py
+++ ERP/ERP/settings.py
@@ -88,7 +88,11 @@ WSGI_APPLICATION = 'ERP.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
  
