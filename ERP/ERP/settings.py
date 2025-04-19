d3f9baea3bb3f3f452e0c93df6f24bde8592b8e6
Atul12396 <atul66017@gmail.com>
2025-04-19 12:35:29 +0530

    Change in database

--- ERP/ERP/settings.py
+++ ERP/ERP/settings.py
@@ -85,13 +85,13 @@ WSGI_APPLICATION = 'ERP.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
+ 
 
 
 # Password validation
